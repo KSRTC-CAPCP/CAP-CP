1b6f962784b8285a8db77ac553555608f4b5a15a
Dharanivasu <IT@Capcp.in>
2023-12-29 15:01:19 +0530

    new updates

--- src/views/Employee/Details/Bank.js
+++ src/views/Employee/Details/Bank.js
@@ -1,4 +1,4 @@
-import { Grid, TextField } from '@mui/material'
+import { Card, Grid, TextField } from '@mui/material'
 import React from 'react'
 
 const Bank = () => {
@@ -10,13 +10,13 @@ const Bank = () => {
                         <TextField label="Bank Name " placeholder="Bank Name " fullWidth required />
                     </Grid>
                     <Grid item xs={3} p={2}>
-                        <TextField label="Bank Branch Name " placeholder="Bank Branch Name" fullWidth  required/>
+                        <TextField label="Bank Branch Name " placeholder="Bank Branch Name" fullWidth required />
                     </Grid>
                     <Grid xs={3} p={2}>
-                        <TextField label="Bank Account Number" placeholder="Bank Account Number" fullWidth  required />
+                        <TextField label="Bank Account Number" placeholder="Bank Account Number" fullWidth required />
                     </Grid>
                     <Grid xs={3} p={2}>
-                        <TextField label="IFSC Code" placeholder="IFSC Code" fullWidth  required/>
+                        <TextField label="IFSC Code" placeholder="IFSC Code" fullWidth required />
                     </Grid>
                     <Grid xs={3} p={2}>
                         <TextField label="EPFO UAN Number" placeholder="EPFO UAN Number" fullWidth />
@@ -26,6 +26,47 @@ const Bank = () => {
                     </Grid>
                 </Grid>
             </div>
+            <div >
+                <Card style={{ width: '50rem', height: '25rem', marginLeft: '15rem', }} >
+                    <div className='d-flex justify-content-between align-iteam-center'>
+                        <h6 className='h5margin' >View</h6>
+                    </div>
+                    <hr />
+                    <div className="name-list" style={{ marginLeft: '6rem' }}>
+                        <Grid className='d-flex align-items-center' container>
+                            <Grid xs={6} style={{ fontWeight: 'bolder', alignItems: 'center' }}>Bank Name</Grid>
+                            <Grid xs={2}  >:</Grid>
+                            <Grid> Indian Bank</Grid>
+                        </Grid>
+                        <Grid className='d-flex align-items-center' container>
+                            <Grid xs={6} style={{ fontWeight: 'bolder', alignItems: 'center' }}> Bank Branch Name</Grid>
+                            <Grid xs={2}  >:</Grid>
+                            <Grid> Ayanampakam</Grid>
+                        </Grid>
+                        <Grid className='d-flex align-items-center' container>
+                            <Grid xs={6} style={{ fontWeight: 'bolder', alignItems: 'center' }}> Bank Account Number</Grid>
+                            <Grid xs={2}  >:</Grid>
+                            <Grid> 123459SSO6145987</Grid>
+                        </Grid>
+                        <Grid className='d-flex align-items-center' container>
+                            <Grid xs={6} style={{ fontWeight: 'bolder', alignItems: 'center' }}> IFSC Code</Grid>
+                            <Grid xs={2}  >:</Grid>
+                            <Grid> 12365478</Grid>
+                        </Grid>
+                        <Grid className='d-flex align-items-center' container>
+                            <Grid xs={6} style={{ fontWeight: 'bolder', alignItems: 'center' }}> EPFO UAN Number</Grid>
+                            <Grid xs={2} >:</Grid>
+                            <Grid>4569587</Grid>
+                        </Grid>
+                        <Grid className='d-flex align-items-center' container>
+                            <Grid xs={6} style={{ fontWeight: 'bolder', alignItems: 'center' }}> ESIC Number</Grid>
+                            <Grid xs={2} >:</Grid>
+                            <Grid>12587654</Grid>
+                        </Grid>
+                    </div>
+
+                </Card></div>
+
         </div>
     )
 }
